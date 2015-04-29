746146afae332e6649a178c2580aae71ca8948ae
Johnny Li <johnnyli91@gmail.com>
2015-04-28 23:34:07 -0700

    Started working on api

---
+++ Baseball/api/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from ..models import *
+
+
+class TeamSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Team
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    team = TeamSerializer(read_only=True)
+
+    class Meta:
+        model = Player
+
+
+class GameSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Game
+
+
+class InningSerializer(serializers.ModelSerializer):
+    game = GameSerializer(read_only=True)
+    team = TeamSerializer(read_only=True)
+
+    class Meta:
+        model = Inning
+
+
+class BatSerializer(serializers.ModelSerializer):
+    player = PlayerSerializer(read_only=True)
+    inning = InningSerializer(read_only=True)
+
+    class Meta:
+        model = Bat
+
+
