7dd596e1b85fb6a0d0d2e5ebdd1bb5c85188312a
Johnny Li <johnnyli91@gmail.com>
2015-05-17 23:02:25 -0700

    added temp output

--- static/app/controller/home-controller.js
+++ static/app/controller/home-controller.js
@@ -4,6 +4,13 @@ angular.module("baseballApp")
     $scope.playBall = function () {
       $http.post("http://localhost:8000/api/games/", {"name": $scope.gameName, "team": [2,3]}).
         success(function (data, status, headers, config) {
+          var result_name = data.name;
+          var score = data.game_score;
+          var score_arr = [];
+          for (var i = 0; i < score.length; i++) {
+            score_arr.push(score[i]["team"]["name"] + ": " + score[i]["score"].toString() + "\n");
+          }
+          $scope.results = result_name + "\n" + score_arr[0] + score_arr[1];
           console.log(data);
         }).
         error(function (data, status, headers, config) {
