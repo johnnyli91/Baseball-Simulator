b85efefedae6fbe48956aa60a2c106865099b0a5
Johnny Li <johnnyli91@gmail.com>
2015-05-19 23:43:08 -0700

    temp game/ tea pages

--- static/app/controller/game-controller.js
+++ static/app/controller/game-controller.js
@@ -1,5 +1,11 @@
 "use strict";
 angular.module("baseballApp")
   .controller("GameController", function ($scope) {
-      //add code here
+    $http.get("http://localhost:8000/api/games").
+      success(function (data, status, headers, config) {
+        $scope.players = data;
+      }).
+      error(function (data, status, headers, config) {
+        console.log(data);
+      })
   })
--- static/app/controller/team-controller.js
+++ static/app/controller/team-controller.js
@@ -1,5 +1,11 @@
 "use strict";
 angular.module("baseballApp")
-  .controller("TeamController", function ($scope) {
-      //add code here
+  .controller("TeamController", function ($scope, $http) {
+      $http.get("http://localhost:8000/api/teams").
+      success(function (data, status, headers, config) {
+        $scope.teams = data;
+      }).
+      error(function (data, status, headers, config) {
+        console.log(data);
+      })
   })
