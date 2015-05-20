e73df7a13a96ac3e1b78196738787c033a1d0b19
Johnny Li <johnnyli91@gmail.com>
2015-05-19 23:37:48 -0700

    players page

--- Baseball/api/urls.py
+++ Baseball/api/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^teams/(?P<pk>\d+)', TeamRetrieveUpdateDestroyAPIView.as_view()),
     url(r'^players/$', PlayerListAPIView.as_view()),
     url(r'^players/create', PlayerCreateAPIView.as_view()),
-    url(r'^players/(?P<pk>\d+)', PlayerRetrieveUpdateDestroyAPIView),
+    url(r'^players/(?P<pk>\d+)', PlayerRetrieveUpdateDestroyAPIView.as_view()),
     url(r'^games/$', GameListCreateAPIView.as_view()),
     url(r'^games/(?P<pk>\d+)', GameRetrieveUpdateDestroyAPIView.as_view()),
     url(r'^innings/$', InningListAPIView.as_view()),
--- static/app/controller/player-controller.js
+++ static/app/controller/player-controller.js
@@ -1,5 +1,11 @@
 "use strict";
 angular.module("baseballApp")
-  .controller("PlayerController", function ($scope) {
-      //add code here
+  .controller("PlayerController", function ($scope, $http) {
+    $http.get("http://localhost:8000/api/players").
+      success(function (data, status, headers, config) {
+        $scope.players = data;
+      }).
+      error(function (data, status, headers, config) {
+        console.log(data);
+      })
   })
