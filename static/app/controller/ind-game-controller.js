ffb65987f401e8c62a4be798147b6d014384bfdd
Johnny Li <johnnyli91@gmail.com>
2015-05-19 23:22:58 -0700

    basic display for individual game stats

--- static/app/controller/ind-game-controller.js
+++ static/app/controller/ind-game-controller.js
@@ -5,6 +5,9 @@ angular.module("baseballApp")
     $http.get("http://localhost:8000/api/games/" + $scope.pk + "/").
       success(function (data, status, headers, config) {
         console.log(data);
+        $scope.gameName = data.name;
+        $scope.scores = data.game_score;
+        $scope.innings = data.inning;
       }).error(function (data, status, headers, config) {
         console.log(data);
       })
