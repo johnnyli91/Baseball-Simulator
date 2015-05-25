d1810f1d42d9d6ca89c50f522fb05a37545b8f5c
Johnny Li <johnnyli91@gmail.com>
2015-05-24 22:04:18 -0700

    changed result page from text to tables

--- static/app/controller/ind-game-controller.js
+++ static/app/controller/ind-game-controller.js
@@ -2,6 +2,7 @@
 angular.module("baseballApp")
   .controller("IndGameController", function ($scope, $routeParams, $http) {
     $scope.pk = $routeParams["pk"];
+    $scope.inningNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     $http.get("http://localhost:8000/api/games/" + $scope.pk + "/").
       success(function (data, status, headers, config) {
         $scope.gameName = data.name;
