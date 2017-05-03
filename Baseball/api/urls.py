adcef69613d285773354506ff317c1056b1678ab
Johnny Li <johnnyli91@gmail.com>
2017-05-03 14:23:31 -0700

    fixed indivual game score display

--- Baseball/api/urls.py
+++ Baseball/api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from views import TeamListCreateAPIView, TeamDetailView, PlayerListAPIView, \
-    PlayerDetailView, GameListCreateAPIView, GameRetrieveUpdateDestroyAPIView, \
+    PlayerDetailView, GameListCreateAPIView, GameDetailView, \
     InningListAPIView, InningCreateAPIView, InningRetrieveUpdateDestroyAPIView, TestBat
 
 urlpatterns = [
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^players/$', PlayerListAPIView.as_view()),
     url(r'^players/(?P<player_id>\d+)', PlayerDetailView.as_view()),
     url(r'^games/$', GameListCreateAPIView.as_view()),
-    url(r'^games/(?P<pk>\d+)', GameRetrieveUpdateDestroyAPIView.as_view()),
+    url(r'^games/(?P<game_id>\d+)', GameDetailView.as_view()),
     url(r'^innings/$', InningListAPIView.as_view()),
     url(r'^innings/create', InningCreateAPIView.as_view()),
     url(r'^innings/(?P<pk>\d+)', InningRetrieveUpdateDestroyAPIView.as_view()),
