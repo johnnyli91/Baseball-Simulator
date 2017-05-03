6bbebfbf9819cb0bbd3555bc74f6856ac9c86b7b
Johnny Li <johnnyli91@gmail.com>
2017-05-03 13:18:34 -0700

    fixed team/ player displays

--- static/app/controller/ind-team-controller.js
+++ static/app/controller/ind-team-controller.js
@@ -5,7 +5,7 @@ angular.module("baseballApp")
     $http.get("/api/teams/" + $scope.pk + "/").
       success(function (data, status, headers, config) {
         $scope.teamName = data.name;
-        $scope.players = data.team_player;
+        $scope.players = data.players;
       }).error(function (data, status, headers, config) {
         console.log(data);
       })
